2f5ff031f8e651a39e6bca4bb7fb96ffcdd898b7
eman-projectspace <emanwaheed001@gmail.com>
2025-02-07 20:15:08 +0500

    Added Carousel

--- src/Baroque/Home.js
+++ src/Baroque/Home.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
 function Home() {
   return (
     <div>
-       <div className='mt-2'>
-      <div className='w-10/12 h-2'></div>
-      <img  className="-mt-20" src="https://baroque.pk/cdn/shop/files/MAin_BAnner93.jpg?v=1738051250&width=2000 " alt=""/>
+       <div>
+      <div className='w-10/12 h-2 mt-2'></div>
+      <img  className="-mt-20" src="https://baroque.pk/cdn/shop/files/MAin_BAnner94.jpg?v=1738656243&width=2000 " alt=""/>
       <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex gap-4">
-      <button className="bg-white text-black px-6 py-2 w-40 ">STITCHED</button>
-    <button className="bg-black text-white px-6 py-2 w-40 ">UNSTITCHED</button>
+      <button className="bg-white text-black px-6 py-2 w-40 mt-20 h-12">STITCHED</button>
+      <button className="bg-black text-white px-6 py-2 w-40  mt-20">UNSTITCHED</button>
       </div>
       </div>
-      <div class="flex my-5">
+      <div class="flex mt-24 mb-3">
   <div class="flex mx-5" >
     <img  src="./Images/flex-1.webp" alt=""/>
 
@@ -24,29 +25,67 @@ function Home() {
   </div>
 </div>
 
-<div class="flex">
+<div class="flex mb-3">
   <div class="ms-20 text-3xl font-serif">CHANTELLE</div>
   <div class="ms-60 text-3xl font-serif">READY TO WEAR</div>
   <div class="ms-60 text-3xl font-serif">SHAWLS</div>
 </div>
 <div class="flex">
-<div class="ms-20"><button class="bg-black w-40">SHOP NOW</button></div>
-<div class="ml-80"><button  class="bg-black w-40">SHOP NOW</button></div>
-<div class="ms-64 me-8"><button  class="bg-black w-40">SHOP NOW</button></div>
+<div><button class="bg-black w-40 h-12 text-white ml-24 mr-72">SHOP NOW</button></div>
+<div><button  class="bg-black w-40 h-12 text-white mr-5">SHOP NOW</button></div>
+<div><button  class="bg-black w-40 h-12 text-white ml-64 mr-20">SHOP NOW</button></div>
 </div>
 <br/><br/><br/><br/>
 <hr/>
 <br/><br/><br/>
-<div class="ml-96 text-4xl ">
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>SHOP&nbsp;&nbsp; THE &nbsp;&nbsp;LOOK</u></div>
-  <br/><br/><br/>
-  <div class="flex mx-40" >
-    <div class="mx-8"><img src="./Images/flex-4.webp" alt=""></img></div>
-    <div class="size-80 h-20 my-20"><img src="./Images/flex-5.webp" alt=""></img>
-    <div className="text-1xl bold">&nbsp;&nbsp;&nbsp;&nbsp;DIGITAL PRINTED KHADDAR<br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;UF-4019<br></br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    PKR 6,400.00</div>
-    <button class="bg-black mx-8">VIEW PRODUCT</button>
-    </div></div>
+<div class="ml-14 text-4xl font-light mb-20 ">
+  <u>SHOP THE LOOK</u></div>
+
+  <Carousel data-bs-theme="dark">
+      <Carousel.Item >
+        <div className='flex'>
+        <img
+          className=" w-50 h-full ml-36 mr-20"
+           src="https://baroque.pk/cdn/shop/files/Shop_The_Look92.jpg?v=1738051812" alt="First slide"/>
+           {/* SMALL IMG 1 */}
+         <div className="h-96 w-56 mt-20">
+        <img src="https://baroque.pk/cdn/shop/files/65_889fa37c-e256-48fc-84b2-52a924288dc7.jpg?v=1737462104&width=600" alt=""></img>
+        <div className='font-serif w-52 mt-2'>EMBROIDERED CHIFFON PR-1056</div>
+        <div className='font-serif'>PKR 29,900.00</div>
+        <button className='bg-black w-44 h-12 text-white mt-3'>VIEW PRODUCT</button>
+         </div>
+    </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='flex'>
+        <img
+          className=" w-50 h-full ml-36 mr-20"
+          src="https://baroque.pk/cdn/shop/files/Shop_The_Look90.jpg?v=1736336986" alt="Second slide"/>
+       {/* SMALL IMG 2 */}
+       <div className="h-96 w-56 mt-20">
+        <img src="https://baroque.pk/cdn/shop/files/88_a84d6f90-d659-4fab-9637-d77cfe27f98a.jpg?v=1735813514" alt=""></img>
+        <div className='font-serif w-52 mt-2'>EMBROIDERED CHIFFON PR-1056</div>
+        <div className='font-serif'>PKR 10,900.00</div>
+        <button className='bg-black w-44 h-12 mt-3 text-white'>VIEW PRODUCT</button>
+  </div>
+    </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='flex'>
+        <img
+          className=" w-50 h-full ml-36 mr-20"
+          src="https://baroque.pk/cdn/shop/files/27_44117e97-c365-4b12-9d32-17b4a9d77f7f.jpg?v=1733309562" alt="Third slide"/>
+         {/* SMALL IMG 3 */}
+         <div className="h-96 w-56 mt-20">
+          <img src="https://baroque.pk/cdn/shop/files/27_44117e97-c365-4b12-9d32-17b4a9d77f7f.jpg?v=1733309562" alt=""></img>
+          <div className='font-serif w-52 mt-2'>EMBROIDERED CHIFFON PR-1056</div>
+        <div className='font-serif'>PKR 29,900.00</div>
+       <button className='bg-black w-44 h-12 text-white mt-3'>VIEW PRODUCT</button>
+</div>
+    </div>
+      </Carousel.Item>
+    </Carousel>
+
   <br/><br/><br/>
   <hr/>
   <br/><br/><br/>
