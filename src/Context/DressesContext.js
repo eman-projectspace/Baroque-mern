3004740f3e12b5ff13f5806be2057dad4b08a5d6
eman-projectspace <emanwaheed001@gmail.com>
2025-02-09 13:24:54 +0500

    Added Context in Index.js

---
+++ src/Context/DressesContext.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const DressesContext = createContext(null);
+export default DressesContext;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import AuthContext from "./Context/AuthContext.js";
+import DressesContext from "./Context/DressesContext.js";
+import CartContext from "./Context/CartContext.js";
+import OrderContext from "./Context/OrderContext.js";
+import AdminContext from "./Context/AdminContext.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContext.Provider value={{ user: null, setUser: () => {} }}>
+      <DressesContext.Provider value={{ dresses: [], setDresses: () => {} }}>
+        <CartContext.Provider value={{ cart: [], setCart: () => {} }}>
+          <OrderContext.Provider value={{ orders: [], setOrders: () => {} }}>
+            <AdminContext.Provider value={{ admin: { isAdmin: false }, setAdmin: () => {} }}>
+              <App />
+            </AdminContext.Provider>
+          </OrderContext.Provider>
+        </CartContext.Provider>
+      </DressesContext.Provider>
+    </AuthContext.Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
