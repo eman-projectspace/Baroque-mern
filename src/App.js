229cf3295b54a94e00e6d299a257eb5e9bb1189a
eman-projectspace <emanwaheed001@gmail.com>
2025-02-18 10:30:06 +0500

    commented App.css code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+// import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Nevigation from './Component/Nevigation';
 import Header from './Component/Header';
